8c8b114d66234c9c502aadfae4f69eb591ae919c
KuroNgo <hoaiphong01012002@gmail.com>
2023-12-31 01:12:13 +0700

    add: concurrency

--- concurrency/goroutine.go
+++ concurrency/goroutine.go
@@ -42,6 +42,7 @@ func Goroutines() {
 		fmt.Println("3. Cách khai báo một Goroutine")
 
 		// hàm ở đây
+		DefineAGoroutine()
 
 	case 4:
 		_os.OperatingSystem()
@@ -73,4 +74,35 @@ func CompareToOSThread() {
 		"\nGoroutines có thể giao tiếp an toàn với nhau thông qua các kênh trong Golang (Channel)." +
 		"\nCác channel hỗ trợ mutex lock vì thế tránh được các lỗi liên quan tới cùng ghi và đọc lên vùng dữ liệu chia sẻ (data race)." +
 		"\nGoroutines có thể được ánh xạ và hoạt động trên nhiều OS threads thay vì là ánh xạ 1:1.")
+}
+
+func DefineAGoroutine() {
+	fmt.Println("Bất kì một hàm nào trong Golang cũng đều có thể chạy đồng thời hay Goroutines với việc thêm vào từ khoá go." +
+		"\nTừ khoá go cũng chính là tên của ngôn ngữ Go, là first-class keyword. " +
+		"\nNghĩa là trong Golang bạn không cần phải import bất kì một package nào để sử dụng nó.")
+
+	fmt.Println("func name(){" +
+		"\n// statements" +
+		"\n}" +
+		"\n\n// using go keyword as the " +
+		"\n// prefix of your function call" +
+		"\ngo name()")
+
+	// goroutine
+	go sayHello("Phong nè")
+
+	// normal function
+	sayHello("Kuro")
+
+	fmt.Println("Trong ví dụ trên, hàm sayHello(\"Phong nè\") có từ khoá go phía trước nên sẽ là một Goroutine. " +
+		"\nHàm này sẽ trả về ngay lập tức chứ không thực thi ngay. " +
+		"\nSau đó hàm sayHello(\"Kuro\") được thực thi bình thường bởi main goroutine. Kết quả chúng ta thấy được \"Hello Kuro\" được in ra 5 lần trên màn hình." +
+		"\n\nVậy tại sao không thấy \"Hello Viet\" nào cả?! Vì khi kết thúc hàm sayHello(\"Kuro\"), " +
+		"\nmain goroutine đã kết thúc và chương trình exit trước khi các Goroutine bên dưới nó được thực thi.")
+}
+
+func sayHello(name string) {
+	for i := 0; i <= 5; i++ {
+		fmt.Printf("Hello %s\n", name)
+	}
 }
